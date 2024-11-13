fecf024965c236c466f0e2a1c9ded0c82e0d94a1
Lore <moyagomez23@iespabloserrano.com>
2024-11-14 00:19:41 +0100

    Actualizado

--- EjerciciosStrings/procesar.php
+++ EjerciciosStrings/procesar.php
@@ -1,20 +1,57 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-$nombre = htmlspecialchars($_POST['nombre']);
-$apellido = htmlspecialchars($_POST['apellido']);
-echo "Nombre: " . $nombre . "<br>";
-echo "Apellido: " . $apellido . "<br>";
-echo "Bienvenido" $nombre $apellido;
-echo strrev($nombre $apellido);
-echo "Tu nombre tiene" str_word_count($nombre) "carácteres";
-echo "Tu apellido tiene" str_word_count($apellido) "carácteres";
-echo "El número total de letras son:" str_word_count($nombre $apellido);
-echo "Tu nombre en mayúsculas es" strtoupper($nombre $apellido);
-echo "La primera letra es" .$nombre[strlen($nombre) -1] "y la última es" .$apellido[strlen($apellido)-1];
-echo "Y tu nombre tras haber cambiado las vocales es:"
+    // Recoger los datos del formulario
+    $nombre = htmlspecialchars($_POST['nombre']);
+    $apellido = htmlspecialchars($_POST['apellido']);
+    
+    // Mensaje de bienvenida
+    echo "Bienvenido, " . $nombre . " " . $apellido . "<br>";
 
+    // Escribir el nombre y apellido al revés
+    echo "Nombre al revés: " . strrev($nombre) . "<br>";
+    echo "Apellido al revés: " . strrev($apellido) . "<br>";
 
+    // Contar las letras del nombre, apellido y total
+    $nombre_len = strlen($nombre);
+    $apellido_len = strlen($apellido);
+    $total_len = $nombre_len + $apellido_len;
+    
+    echo "Número de letras en el nombre: " . $nombre_len . "<br>";
+    echo "Número de letras en el apellido: " . $apellido_len . "<br>";
+    echo "Número total de letras: " . $total_len . "<br>";
 
-echo var_dump($nombre);
+    // Escribir el nombre y apellido en mayúsculas
+    echo "Nombre en mayúsculas: " . strtoupper($nombre) . "<br>";
+    echo "Apellido en mayúsculas: " . strtoupper($apellido) . "<br>";
+
+    // Primer y último carácter del nombre y apellido
+    echo "Primer carácter del nombre: " . $nombre[0] . "<br>";
+    echo "Último carácter del nombre: " . $nombre[$nombre_len - 1] . "<br>";
+    echo "Primer carácter del apellido: " . $apellido[0] . "<br>";
+    echo "Último carácter del apellido: " . $apellido[$apellido_len - 1] . "<br>";
+
+    // Cambiar todas las vocales del nombre y apellido por la letra 'u'
+    $nombre_modificado = str_ireplace(['a', 'e', 'i', 'o', 'u'], 'u', $nombre);
+    $apellido_modificado = str_ireplace(['a', 'e', 'i', 'o', 'u'], 'u', $apellido);
+    
+    echo "Nombre con vocales cambiadas por 'u': " . $nombre_modificado . "<br>";
+    echo "Apellido con vocales cambiadas por 'u': " . $apellido_modificado . "<br>";
+
+    // Operaciones con los números
+    if (isset($_POST['num1']) && isset($_POST['num2'])) {
+        $num1 = (float) $_POST['num1'];
+        $num2 = (float) $_POST['num2'];
+
+        // Mostrar operaciones
+        echo "<br>Operaciones con los números:<br>";
+        echo "Suma: " . ($num1 + $num2) . "<br>";
+        echo "Diferencia: " . ($num1 - $num2) . "<br>";
+        echo "Producto: " . ($num1 * $num2) . "<br>";
+        if ($num2 != 0) {
+            echo "Cociente: " . ($num1 / $num2) . "<br>";
+        } else {
+            echo "No se puede dividir por 0.<br>";
+        }
+    }
 }
 ?>
